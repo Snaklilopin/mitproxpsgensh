42ac991b05df469a9fa15f7038dd16c53aa50967
Snaklilopin <68654691+Snaklilopin@users.noreply.github.com>
2022-05-17 17:19:56 +0300

    add some sleep for reading text

--- installmit.sh
+++ installmit.sh
@@ -1,10 +1,12 @@
 echo Check if you have the latest version of Termux terminal installed for this script and all further proxies/programs to work correctly. This script was made especially for those who want to join non-developer cognosphere private game servers. All rights reserved by their respective owners and used in fair use rights, further questions direct snaklilopin@megaeblan228.gq
+sleep 10
 apt update && apt full-upgrade -y
 pkg install python nano wget rustc-dev -y
 python3 -m ensurepip --upgrade
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
 echo Maybe not working.. idk for what this not correctly export cargo build
+sleep 5
 export CARGO_BUILD_TARGET=aarch64-linux-android
 pipx install mitmproxy
 wget https://raw.githubusercontent.com/Grasscutters/Grasscutter/stable/proxy_config.py
